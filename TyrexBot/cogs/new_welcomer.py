0442598cd65f1379fa749b246aa112f5a6fef889
Alex0w21 <121735548+Alexow021@users.noreply.github.com>
2023-02-08 15:10:26 -0800

    Update new_welcomer.py

--- TyrexBot/cogs/new_welcomer.py
+++ TyrexBot/cogs/new_welcomer.py
@@ -25,15 +25,15 @@ class welcomer(commands.Cog):
 
 
         #welcomer
-        ch = await self.bot.fetch_channel(your channel id here)
+        ch = await self.bot.fetch_channel(your channel id here) #your channel id here
         embed = discord.Embed(title="Welcome to the Club :slight_smile:" , color=discord.Color.random())
         embed.add_field(name=f"{member} joined at {member.joined_at.date()}" , value="feel free in our server to get help use: ```*help```")
         embed.add_field(name=f" " , value=f"from {ch.mention} choose your languege",inline= False)
         embed.set_thumbnail(url=member.avatar)
         embed.add_field(name=f"is bot ? : " , value=f"{member.bot}",inline= False)
         embed.add_field(name=f"account created at : ", value=f"{member.created_at}", inline= False)
-        await self.bot.get_channel(your channel id here).send(f"{member.mention}")
-        await self.bot.get_channel(your channel id here).send(embed = embed)
+        await self.bot.get_channel(your channel id here).send(f"{member.mention}") #your channel id here
+        await self.bot.get_channel(your channel id here).send(embed = embed) #your channel id here
 
 
 
