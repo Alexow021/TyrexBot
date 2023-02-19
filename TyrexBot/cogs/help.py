048c8dc52657a4e625ccf7cba50be8440664e5a7
Alex0w21 <121735548+Alexow021@users.noreply.github.com>
2023-02-19 10:05:42 -0800

    Update help.py

--- TyrexBot/cogs/help.py
+++ TyrexBot/cogs/help.py
@@ -13,13 +13,16 @@ class help(commands.Cog):
     @commands.command(name='help' , aliases = ['INFO' , 'Info' , 'Help' , 'HELP'])
     async def help(self,msg):
         embed = discord.Embed(title=f"Ty-Rex Bot" , color=discord.Color.random())
-        embed.add_field(name='*music' , value='<a:musicbeat:1067921839306854470> Display information about music commands')
-        embed.add_field(name='*other',value="<:commands:1067925738596081765> Display other cool commands")
+        embed.add_field(name='*music' , value='<a:musicplaying:1069771651568381973> Display information about music commands')
+        embed.add_field(name='*other',value="<:commands:1067925738596081765> Display other commands" , inline=False)
+        embed.add_field(name='*galary',value="<:picture:1075288458907615312> Display galary commands"  , inline=False)
         await msg.reply(embed = embed)
 
 
 
-#setup info cog
+
+
+#setting up our cog
 async def setup(bot):
     await bot.add_cog(help(bot))
 
