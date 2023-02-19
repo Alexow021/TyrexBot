caff6d966adab5489d2579101bae307aab616c59
Alex0w21 <121735548+Alexow021@users.noreply.github.com>
2023-02-19 10:06:01 -0800

    Update latancy.py

--- TyrexBot/cogs/latancy.py
+++ TyrexBot/cogs/latancy.py
@@ -10,9 +10,10 @@ class latancy(commands.Cog):
     #latancy of the bot
     @commands.command(name="ping",aliases = ['latancy','PING','late'])
     async def ping(self, msg):
-        await msg.reply(embed = discord.Embed(title="latancy :satellite:" , description=f"latancy of the bot : {round(self.bot.latency * 1000)}ms" , color=discord.Color.random()))
+        await msg.reply(embed = discord.Embed(title="latancy" , description=f"latancy of the bot : {round(self.bot.latency * 1000)}ms <a:botping:1069771709466558514>" , color=discord.Color.random()))
 
 
 
+#setting up our cog
 async def setup(bot):
     await bot.add_cog(latancy(bot))
